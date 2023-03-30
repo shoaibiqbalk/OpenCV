84bc7e31e31ff30dc494cebe5fcfa514ab0178d0
Shoaib Iqbal Khan <shoaibiqbalkhan1@gmail.com>
2023-03-30 07:32:10 +0500

    chapter 4 Added

--- 04-Chapter04.py
+++ 04-Chapter04.py
@@ -7,3 +7,13 @@ from cv2 import cvtColor
 img = cv.imread('resources/image-1.jpg')
 img = cv.resize(img, (800, 600))
 
+gray_img =  cvtColor(img, cv.COLOR_BGR2GRAY)
+
+(thresh, b_w) = cv.threshold(gray_img, 127, 255, cv.THRESH_BINARY)
+
+cv.imshow('First Image', img)
+cv.imshow('Gray Image', gray_img)
+cv.imshow('Gray Image', b_w)
+
+cv.waitKey(0)
+cv.destroyAllWindows()

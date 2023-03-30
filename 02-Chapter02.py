9d2d6715359f83b67730c850cfd86b8fee69c53d
Shoaib Iqbal Khan <shoaibiqbalkhan1@gmail.com>
2023-03-30 07:03:25 +0500

    chapter 2

---
+++ 02-Chapter02.py
@@ -0,0 +1,10 @@
+## Reading an Image and Displaying it
+
+# library import
+import cv2 as cv
+
+img = cv.imread('resources/image-1.jpg')
+img = cv.resize(img, (800, 600)) 
+cv.imshow('First Image', img)
+
+cv.waitKey(0)

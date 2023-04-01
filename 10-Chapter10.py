b9eae5b9c4552ac667faa80eaac34711d2e8689e
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-01 09:26:02 +0500

    Chapter 10 updated

--- 10-Chapter10.py
+++ 10-Chapter10.py
@@ -14,7 +14,10 @@ while True:
     height = int(cap.get(4))
 
     img = cv.line(frame, (0, 0), (width, height), (255, 0, 0), 10) # Drawing line
-    img = cv.rectangle(frame, (100, 100), (200, 200), (200, 0, 0), 5) # Drawing Rectangle
+    img = cv.rectangle(img, (100, 100), (200, 200), (200, 0, 0), 5) # Drawing Rectangle
+    img = cv.circle(img, (300, 300), 60, (0, 0, 255), 5) # Drawing Circle
+    font = cv.FONT_HERSHEY_COMPLEX
+    img = cv.putText(img, 'This is Text', (10, height-10), font, 2, (0, 0, 0), 4, cv.LINE_AA) # Text
 
     cv.imshow('Frame', img)
 

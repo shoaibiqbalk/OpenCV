b01ffd83bbdc91f97ef3cfa2d6cef325a6f4ed89
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-01 08:22:16 +0500

    Chapter 8 added

---
+++ 07-Chapter7.py
@@ -0,0 +1,19 @@
+# Copying & Pasting Parts of the image
+
+## Image manipulation using pixels
+
+import cv2 as cv
+import random
+
+img = cv.imread('resources/image-9.jpg')
+img = cv.resize(img, (800, 600)) 
+cv.imshow('Image', img)
+
+tag = img[500:400, 600:300] # Copy a square of given pixel
+# img[100:300, 650:350] = tag
+
+
+
+cv.imshow('Manipulated image', tag)        
+cv.waitKey(0)
+cv.destroyAllWindows()
---
+++ 08-Chapter8.py
@@ -0,0 +1,12 @@
+# Cam Image
+
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv.imshow('Frame', frame)
+
+    if cv.waitKey(2) == ord('q'):
+        break

9bd792239e09ea7dd2946b92e55a12fe3e46cb90
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-02 12:12:39 +0500

    Chapter 12 updated

--- 12-Chapter12.py
+++ 12-Chapter12.py
@@ -5,8 +5,11 @@ import numpy as np
 
 img = cv.imread('resources/chessboard.png')
 img = cv.resize(img, (0, 0), fx=0.5, fy=0.5)
+img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
 
 cv.imshow('Image', img)
+
 
 cv.waitKey(0)
 cv.destroyAllWindows()

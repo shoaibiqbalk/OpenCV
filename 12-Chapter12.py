fdf5a0049d995fdc93f716bed9d832b9b3f3f380
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-02 12:33:17 +0500

    Chapter 12 updated

--- 12-Chapter12.py
+++ 12-Chapter12.py
@@ -1,26 +1,26 @@
 # Corner Detection
 
-import cv2 as cv
 import numpy as np
+import cv2
 
-img = cv.imread('resources/chessboard.png')
-img = cv.resize(img, (0, 0), fx=0.5, fy=0.5)
-gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img = cv2.imread('resources/chessboard.png')
+img = cv2.resize(img, (0, 0), fx=0.75, fy=0.75)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-corners = cv.goodFeaturesToTrack(gray_img, 100, 0.5, 10)
+corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
 corners = np.int0(corners)
 
 for corner in corners:
-    x, y = corner.ravel()
-    cv.circle(img, (x, y), 5, (255, 0, 0), -1)
+	x, y = corner.ravel()
+	cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
 
 for i in range(len(corners)):
-    for j in range(i+1, len(corners)):
-        corner1 = tuple(corners[i][0])
-        corner2 = tuple(corners[j][0])
-        color = tuple(map(lambda x: int(x), np.random.randint(0, 255, size=3)))
-        cv.line(img, corner1, corner2, (), 1)
+	for j in range(i + 1, len(corners)):
+		corner1 = tuple(corners[i][0])
+		corner2 = tuple(corners[j][0])
+		color = tuple(map(lambda x: int(x), np.random.randint(0, 255, size=3)))
+		cv2.line(img, corner1, corner2, color, 1)
 
-cv.imshow('Image', img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+cv2.imshow('Frame', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

784015927fb2f0a3657ec2d267e44253e737a3de
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-06 14:35:02 +0500

    Chapter 15 updated

--- 15-Chapter15.py
+++ 15-Chapter15.py
@@ -2,9 +2,21 @@
 
 import cv2 as cv
 
-vid = cv.VideoCapture('resource/video.mp4')
+vid = cv.VideoCapture('resources/video.mp4')
 
+# Indicator
 if vid.isOpened() == False:
     print("Error in reading video")
 
-    
+# Reading and playing
+
+while vid.isOpened():
+    ret, frame = vid.read()
+    if ret == True:
+        cv.imshow("Video", frame)
+
+    if cv.waitKey(2) == ord('q'):
+        break
+
+vid.release()
+cv.destroyAllWindows()

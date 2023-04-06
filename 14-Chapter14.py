5c126770bd738e15eb0246657c9da72b717c9589
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-06 12:48:49 +0500

    Chapter 14 updated

--- 14-Chapter14.py
+++ 14-Chapter14.py
@@ -5,9 +5,14 @@ import numpy as np
 
 cap = cv.VideoCapture(0)
 
+# pre-trained classifier models 
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_eye.xml")
+
+
 while True:
     ret, frame = cap.read()
-
+    cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     cv.imshow('Frame', frame)
 
     if cv.waitKey(2) == ord('q'):

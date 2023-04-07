adab5b59e41c37f7ca57cdaba5c60beb3ae745e1
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-07 10:40:51 +0500

    Chapter 03 updated

--- 03-Chapter03.py
+++ 03-Chapter03.py
@@ -6,7 +6,7 @@ import numpy as np
 img = cv.imread('resources/image-1.jpg')
 
 # Resizing
-img = cv.resize(img, (800, 600))
+resized_img = cv.resize(img, (800, 600))
 
 # Grayscale
 gray_img =  cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -24,13 +24,19 @@ dilated_img = cv.dilate(edge_img, (mat_kernal), iterations=1)
 # Eroded Image - Thinnes of edge lines
 ero_img = cv.erode(edge_img, mat_kernal, iterations=1)
 
+# Cropping Image - Using numpy, not OpenCV
+print(f"The size of the Original image is {img.shape}")
+cropped_img = resized_img[0:200, 0:300]   
 
-cv.imshow('First Image', img)
+
+cv.imshow('Original Image', img)
+cv.imshow('Resized Image', resized_img)
 cv.imshow('Gray Image', gray_img)
 cv.imshow('Blurred Image', blurr_img)
 cv.imshow('Edge Image', edge_img)
 cv.imshow('Dilated Image', dilated_img)
 cv.imshow('Eroded Image', ero_img)
+cv.imshow('Cropped Image', cropped_img)
 
 
 cv.waitKey(0)

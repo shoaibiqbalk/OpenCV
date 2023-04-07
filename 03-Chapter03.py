a3a834c9068bbc298dfa39d58e2f00325bbe9f5f
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-07 10:42:07 +0500

    Chapter 03 updated

--- 03-Chapter03.py
+++ 03-Chapter03.py
@@ -9,13 +9,13 @@ img = cv.imread('resources/image-1.jpg')
 resized_img = cv.resize(img, (800, 600))
 
 # Grayscale
-gray_img =  cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+gray_img =  cv.cvtColor(resized_img, cv.COLOR_BGR2GRAY)
 
 # Blurred Image
-blurr_img = cv.GaussianBlur(img, (7, 7), 0)
+blurr_img = cv.GaussianBlur(resized_img, (7, 7), 0)
 
 # Edge Detection
-edge_img = cv.Canny(img, 48, 48)
+edge_img = cv.Canny(resized_img, 48, 48)
 
 # Dilated Image - Thickness of edge lines
 mat_kernal = np.ones((7, 7), np.uint8)

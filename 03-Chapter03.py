25a6d90171f5c61ba0ba182210cb9eda9df542cf
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-07 10:33:56 +0500

    Chapter 03 updated

--- 03-Chapter03.py
+++ 03-Chapter03.py
@@ -1,6 +1,7 @@
 ## Basic Manipulation of Images
 
 import cv2 as cv
+import numpy as np
 
 img = cv.imread('resources/image-1.jpg')
 
@@ -16,11 +17,21 @@ blurr_img = cv.GaussianBlur(img, (7, 7), 0)
 # Edge Detection
 edge_img = cv.Canny(img, 48, 48)
 
+# Dilated Image - Thickness of edge lines
+mat_kernal = np.ones((7, 7), np.uint8)
+dilated_img = cv.dilate(edge_img, (mat_kernal), iterations=1)
+
+# Eroded Image - Thinnes of edge lines
+ero_img = cv.erode(edge_img, mat_kernal, iterations=1)
+
 
 cv.imshow('First Image', img)
 cv.imshow('Gray Image', gray_img)
 cv.imshow('Blurred Image', blurr_img)
 cv.imshow('Edge Image', edge_img)
+cv.imshow('Dilated Image', dilated_img)
+cv.imshow('Eroded Image', ero_img)
+
 
 cv.waitKey(0)
 cv.destroyAllWindows()

2d23bd62ebb5510da82efc63446eb87a78a15def
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-09 06:19:11 +0500

    Chapter 21 updated

--- 21-Chapter21.py
+++ 21-Chapter21.py
@@ -7,11 +7,22 @@ img = cv.imread('resources/image-1.jpg')
 
 # Co-ordinate finding fuction
 def find_coord(event, x, y, flags, params):
-    if event == cv.EVENT_FLAG_LBUTTON:
+    if event == cv.EVENT_LBUTTONDOWN:
         print("{} {}".format(x, y)) # left mouse click 
         # defining or printing on the same image or window 
         font = cv.FONT_HERSHEY_PLAIN
         cv.putText(img, "{} {}".format(x, y), (x, y), font, 1, (255, 0, 179), thickness=2)
+        cv.imshow("Image", img)
+
+    # for color finding
+    if event == cv.EVENT_RBUTTONDOWN:
+        print("{} {}".format(x, y)) # left mouse click 
+        font = cv.FONT_HERSHEY_SIMPLEX
+        b = img[y, x, 0]
+        g = img[y, x, 1]
+        r = img[y, x, 2]
+
+        cv.putText(img, "{} {} {}".format(b, g, r), (x, y), font, 1, (255, 0, 0), 2)
         cv.imshow("Image", img)
 
 

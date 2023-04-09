17a37284e7f5cd2e6992655a0d4e17649dc533a3
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-09 14:39:02 +0500

    Chapter 23 updated

--- 23-Chapter23.py
+++ 23-Chapter23.py
@@ -31,6 +31,7 @@ cv.createTrackbar("Val Max", "Bars", 255, 255, slider)
 
 while True:
     img = cv.imread(path)
+    img = cv.resize(img, (800, 600))
     hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
     hue_min = cv.createTrackbar("Hue Min", "Bars", 0, 179, slider)
@@ -46,10 +47,16 @@ while True:
 
     # changes inside an image
     lower = np.array([hue_min, sat_min, val_min])
-    lower = np.array([hue_max, sat_max, val_max])     
+    upper = np.array([hue_max, sat_max, val_max])     
 
-    cv.imshow('Image', img)
-    cv.imshow('HSV Image', hsv_img)
+    mask_img = cv.inRange(hsv_img, lower, upper)
+    out_img = cv.bitwise_and(img, img, mask=mask_img)
+
+    # cv.imshow('Image', img)
+    # cv.imshow('HSV Image', hsv_img)
+    cv.imshow('Masked Image', mask_img)
+    cv.imshow('Output Image', out_img)
+
     if cv.waitKey(2) & 0xFF == ord('q'):
             break
 

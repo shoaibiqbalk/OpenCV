0d075f47c22c9148a564889a04a3e12dc8ffe23c
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-09 06:08:43 +0500

    Chapter 21 updated

--- 21-Chapter21.py
+++ 21-Chapter21.py
@@ -5,3 +5,19 @@ import numpy as np
 
 img = cv.imread('resources/image-1.jpg')
 
+# Co-ordinate finding fuction
+def find_coord(event, x, y, flags, params):
+    if event == cv.EVENT_FLAG_LBUTTON:
+        print("{} {}".format(x, y)) # left mouse click 
+        # defining or printing on the same image or window 
+        font = cv.FONT_HERSHEY_PLAIN
+        cv.putText(img, "{} {}".format(x, y), (x, y), font, 1, (255, 0, 179), thickness=2)
+        cv.imshow("Image", img)
+
+
+# Function to read and display
+if __name__ == "__main__":
+    img = cv.imread
+
+
+

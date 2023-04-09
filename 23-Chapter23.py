bbe24bfd2a1ae4905c7e701438b303c49b97aa88
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-09 14:28:36 +0500

    Chapter 23 updated

--- 23-Chapter23.py
+++ 23-Chapter23.py
@@ -4,6 +4,7 @@ import cv2 as cv
 import numpy as np
 
 img = cv.imread('resources/image-1.jpg') 
+img = cv.resize(img, (800, 600))
 
 # Convert ot HSV - (Hue, Value Saturation)
 hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
@@ -13,18 +14,43 @@ def slider():
     pass
 
 path = "resources/image-1.jpg"
+
+ 
 cv.namedWindow("Bars")
 cv.resizeWindow("Bars", 640, 300)
 
-cv.createTrackbar("Hue Minimun", "Bars", 0, 179, slider)
-cv.createTrackbar("Hue Maximum", "Bars", 179, 179, slider)
+cv.createTrackbar("Hue Min", "Bars", 0, 179, slider)
+cv.createTrackbar("Hue Max", "Bars", 179, 179, slider)
 
-cv.createTrackbar("Value Minimun", "Bars", 0, 179, slider)
-cv.createTrackbar("Value Maximum", "Bars", 179, 179, slider)
+cv.createTrackbar("Sat Min", "Bars", 0, 255, slider)
+cv.createTrackbar("Sat Max", "Bars", 255, 255, slider)
 
-cv.createTrackbar("Saturation Minimun", "Bars", 0, 179, slider)
-cv.createTrackbar("Saturation Maximum", "Bars", 179, 179, slider)
+cv.createTrackbar("Val Min", "Bars", 0, 255, slider)
+cv.createTrackbar("Val Max", "Bars", 255, 255, slider)
 
-cv.imshow('Image', img)
-cv.waitKey(0)
+
+while True:
+    img = cv.imread(path)
+    hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+    hue_min = cv.createTrackbar("Hue Min", "Bars", 0, 179, slider)
+    hue_max = cv.createTrackbar("Hue Max", "Bars", 179, 179, slider)
+
+    sat_min = cv.createTrackbar("Sat Min", "Bars", 0, 255, slider)
+    sat_max =cv.createTrackbar("Sat Max", "Bars", 255, 255, slider)
+
+    val_min = cv.createTrackbar("Val Min", "Bars", 0, 255, slider)
+    val_max = cv.createTrackbar("Val Max", "Bars", 255, 255, slider)
+
+    print(hue_min, hue_max, sat_min, sat_max, val_min, val_max)
+
+    # changes inside an image
+    lower = np.array([hue_min, sat_min, val_min])
+    lower = np.array([hue_max, sat_max, val_max])     
+
+    cv.imshow('Image', img)
+    cv.imshow('HSV Image', hsv_img)
+    if cv.waitKey(2) & 0xFF == ord('q'):
+            break
+
 cv.destroyAllWindows()

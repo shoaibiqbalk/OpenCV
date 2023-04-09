b84381ce7f8e8b9253d5cafb76e24489dbc3606c
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-09 12:54:45 +0500

    Chapter 22 updated

--- 22-Chapter22.py
+++ 22-Chapter22.py
@@ -9,4 +9,10 @@ frameNr = 0
 while True:
     success, frame = vid.read()
     if success:
-        
+        cv.imwrite(f"resource/frame/frame_{frameNr}.jpg", frame)
+    else:
+        break
+
+    frameNr += 1
+
+vid.release()

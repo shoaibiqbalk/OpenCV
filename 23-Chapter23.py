9f4d71872c1bcda1f51aaef3e516b3f41c1fe490
Shoaib Iqbal Khan <sk721638@gmail.com>
2023-04-09 13:19:12 +0500

    Chapter 23 Added

--- 23-Chapter23.py
+++ 23-Chapter23.py
@@ -5,6 +5,25 @@ import numpy as np
 
 img = cv.imread('resources/image-1.jpg') 
 
+# Convert ot HSV - (Hue, Value Saturation)
+hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+# Sliders
+def slider():
+    pass
+
+path = "resources/image-1.jpg"
+cv.namedWindow("Bars")
+cv.resizeWindow("Bars", 640, 300)
+
+cv.createTrackbar("Hue Minimun", "Bars", 0, 179, slider)
+cv.createTrackbar("Hue Maximum", "Bars", 179, 179, slider)
+
+cv.createTrackbar("Value Minimun", "Bars", 0, 179, slider)
+cv.createTrackbar("Value Maximum", "Bars", 179, 179, slider)
+
+cv.createTrackbar("Saturation Minimun", "Bars", 0, 179, slider)
+cv.createTrackbar("Saturation Maximum", "Bars", 179, 179, slider)
 
 cv.imshow('Image', img)
 cv.waitKey(0)
